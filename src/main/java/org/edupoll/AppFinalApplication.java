55cd5c1ee28d5c30d87fcd286fa5ba32b770808c
qwert0319 <mpo0815@naver.com>
2023-07-20 13:57:37 +0900

    feat: final

---
+++ src/main/java/org/edupoll/AppFinalApplication.java
@@ -0,0 +1,14 @@
+package org.edupoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFinalApplication.class, args);
+	}
+
+}
+   
